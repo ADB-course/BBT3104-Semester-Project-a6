3b8acf2e425383dddd367bafa59f833c28be22d3
Ivy Tanui <ivy.tanui@strathmore.edu>
2024-11-16 22:07:53 +0300

    Adding Supplier ID Positive Value Constraint

--- 4_check_constraints/script.sql
+++ 4_check_constraints/script.sql
@@ -3,3 +3,8 @@
 ALTER TABLE products 
 ADD CONSTRAINT chk_product_availability 
 CHECK (product_available IN ('Yes', 'No'));
+--Supplier ID Positive Value Constraint
+ALTER TABLE suppliers 
+ADD CONSTRAINT chk_supplier_id_positive 
+CHECK (supplierID > 0);
+
