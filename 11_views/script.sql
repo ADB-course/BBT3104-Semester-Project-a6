e751aecdc028813eed67d685a9b703cf246717cb
irenekariuki <144525530+irenekariuki@users.noreply.github.com>
2024-11-17 12:01:16 +0300

    select from statement in views

--- 11_views/script.sql
+++ 11_views/script.sql
@@ -1,2 +1,11 @@
 -- Write your SQL code here
 CREATE VIEW EmployeeCommunicationOverview AS
+SELECT
+    employee.employee_id,
+    employee.employee_name,
+    communication_log.communication_code,
+    communication_log.date AS communication_date,
+    commun ication_log.message_details,
+    supplier.supplier_name
+FROM
+    employee   
