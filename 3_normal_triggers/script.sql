325af03a4d62f011bc6ea0fd571f18561c315e3a
waniniRan <cindy.gichuhi@strathmored.edu>
2024-11-17 11:48:20 +0300

    Create a trigger

--- 3_normal_triggers/script.sql
+++ 3_normal_triggers/script.sql
@@ -1,1 +1,26 @@
 -- Write your SQL code here
+Delimiter$$
+
+CREATE TRIGGER TRG_B_Insert_products  
+BEFORE 
+INSERT 
+ON  
+products 
+
+INSERT INTO 
+`productID` = OLD.`productID` ,
+`product_name` =OLD.`product_name`,
+`product_group` =OLD.`product_group` ,
+`product_available` =OLD.`product_available` ,
+`supplierID`=OLD.`supplierID` ,
+`supplier_name` =OLD.`supplier_name`, 
+
+
+
+
+
+
+
+
+END
+
