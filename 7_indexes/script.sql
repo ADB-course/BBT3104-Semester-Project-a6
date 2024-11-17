8a9754b50202878946feb73c1fb399ea9443611c
fatumaahmed2024 <Fatuma.Mohamed@strathmore.edu>
2024-11-17 20:06:03 +0300

    Indexing on the last entities

--- 7_indexes/script.sql
+++ 7_indexes/script.sql
@@ -1,6 +1,6 @@
 --1.PROCUREMENT ENTITY
 -- B+ Tree index on Procurement department employee code
-CREATE INDEX idx_procurement_employee_code
+CREATE UNIQUE INDEX idx_procurement_employee_code
 ON Procurement (ProcurementDepartmentEmployeeCode);
 
 -- B+ Tree index on Employee ID
@@ -40,8 +40,9 @@ CREATE INDEX idx_employee_department
 ON Employee (Department);
 
 -- B+ Tree index on Employee phone number
-CREATE UNIQUE INDEX idx_employee_phone_number
+CREATE INDEX idx_employee_phone_number
 ON Employee (EmployeePhoneNumber);
+
 
 ----4.Communication Log ENTITY
 -- B+ Tree index on Communication log code
@@ -64,3 +65,38 @@ ON CommunicationLog (SupplierID);
 CREATE FULLTEXT INDEX ON CommunicationLog (Details)
 KEY INDEX idx_communication_log_code;
 
+
+---5.Supplier Liaison Entity
+-- B+ Tree index on Supplier liaison code
+CREATE UNIQUE INDEX idx_supplier_liaison_code 
+ON SupplierLiaison (SupplierLiaisonCode);
+
+-- B+ Tree index on Employee ID 
+CREATE INDEX idx_liaison_employee_id 
+ON SupplierLiaison (EmployeeID);
+
+-- B+ Tree index on Supplier ID 
+CREATE INDEX idx_liaison_supplier_id 
+ON SupplierLiaison (SupplierID);
+
+-- B+ Tree index on Communication log code
+CREATE INDEX idx_liaison_communication_log_code 
+ON SupplierLiaison (CommunicationLogCode);
+
+-- B+ Tree index on Date of communication
+CREATE INDEX idx_liaison_date 
+ON SupplierLiaison (DateOfCommunication);
+
+
+--6.Product Entity
+-- B+ Tree index on Product ID 
+CREATE UNIQUE INDEX idx_product_id 
+ON Product (ProductID);
+
+-- B+ Tree index on Product group
+CREATE INDEX idx_product_group 
+ON Product (ProductGroup);
+
+-- B+ Tree index on Supplier ID 
+CREATE INDEX idx_product_supplier_id 
+ON Product (SupplierID);
