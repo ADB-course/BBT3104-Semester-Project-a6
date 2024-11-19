7004a62d710f9d713fd5876487f72436cd169a82
Ivy Tanui <ivy.tanui@strathmore.edu>
2024-11-19 09:00:23 +0300

    Update the Supplier ID check constraint

--- 4_check_constraints/script.sql
+++ 4_check_constraints/script.sql
@@ -1,5 +1,6 @@
 -- Write your SQL code here
---
+--Product availability
 ALTER TABLE products 
 ADD CONSTRAINT chk_product_group 
 CHECK (product_group IN ('Electronics', 'Furniture', 'Groceries'));
+--
