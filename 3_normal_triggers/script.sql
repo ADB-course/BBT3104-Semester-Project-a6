0fd924d66a5f16f2231708ecd00791e76ea5b946
waniniRan <cindy.gichuhi@strathmored.edu>
2024-11-20 21:45:32 +0300

    Changed triggers

--- 3_normal_triggers/script.sql
+++ 3_normal_triggers/script.sql
@@ -1,52 +1,49 @@
 -- Write your SQL code here
 
+/* The first two triggers is to first:insert new products in the inventory and the second trigger is 
+for making changes if the number of stock changes in anyway, (through any sale or maybe a refund) */
+
+
+
 CREATE TRIGGER TRG_Before_Insert_Products  
 BEFORE INSERT ON  products 
         FOR EACH ROW
 BEGIN
 
-   IF NEW.product_available IS NULL THEN
-    SET NEW.product_available= 0;
-
+   IF NEW.stock IS NULL THEN
+   SET NEW.stock = 0;
+   
    END IF;
+   
+ END
 
 
-CREATE TRIGGER TRG_After_Insert_Products
-AFTER INSERT ON products
-FOR EACH ROW
+CREATE TRIGGER TRG_After_Update_Products
+AFTER UPDATE ON products
+       FOR EACH ROW
 BEGIN
+    
+    INSERT INTO products (productID, product_name, product_group,product_available)
+    VALUES ( OLD.productID,OLD.product_name,OLD.product_group,OLD.product_available,NEW.product_available); 
 
-    INSERT INTO products (productID, product_name, product_available)
-    VALUES (NEW.productID, NEW.product_name, NEW.product_available);
 END;
 
-DELIMITER $$
 
-CREATE TRIGGER TRG_before_insert_communication_log
+/*triggers for communication logs */
+
+CREATE TRIGGER TRG_Before_Insert_communication_log
 BEFORE INSERT ON Communication logs
-FOR EACH ROW
+      FOR EACH ROW
 BEGIN
-   -- Set the current timestamp if the timestamp is not provided
-   IF NEW.current_timestamp IS NULL THEN
-      SET NEW.current_timestamp = NOW();
-   END IF;
-END $$
-
-DELIMITER ;
-
-
+   
+   IF NEW.date_of_communication IS NULL THEN
+      SET NEW.date_of_communication = NOW();
+   
+    END IF;
+    
+END;
 
 
-/*CREATE TRIGGER TRG_Before_Upate_Products
-BEFORE UPDATE ON products
-  FOR EACH ROW
-
-  BEGIN 
-  --stock level is above zero
-
-  IF NEW.product_available < 0 THEN
-     SET NEW.product_available =0;
-     END IF;//
 
 
 
