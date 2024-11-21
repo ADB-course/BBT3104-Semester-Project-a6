f34e6bc29aa3843561bcb315fa8b36ebec0e520f
Ivy Tanui <ivy.tanui@strathmore.edu>
2024-11-21 19:40:34 +0300

    Resolved merge conflicts

--- 4_check_constraints/script.sql
+++ 4_check_constraints/script.sql
@@ -1,8 +1,9 @@
 -- Write your SQL code here
 --Phone Number Area Code Constraint
-ALTER TABLE product_info
-ADD CONSTRAINT chk_product_available
-CHECK (product_available IN ('Yes', 'No'));
+ALTER TABLE employee_phone_number
+ADD CONSTRAINT chk_phone_number_format
+CHECK (employee_phone_number REGEXP '^\\+254[0-9]{9}$');
+
 
 --Valid Communication Log Date Constraint
 ALTER TABLE communication_log
