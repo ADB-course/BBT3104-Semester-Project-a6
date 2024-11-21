bf06addb3aa26e8b3ffcd73be8a8d08637979ff7
irenekariuki <144525530+irenekariuki@users.noreply.github.com>
2024-11-21 13:57:42 +0300

    select on second view

--- 11_views/script.sql
+++ 11_views/script.sql
@@ -2,11 +2,11 @@
 -- View for employee communication overview
 CREATE VIEW EmployeeCommunicationOverview AS
 SELECT
-    employee.employee_id,
+    employee.employeeID,
     employee.employee_name,
-    communication_log.communication_code,
-    communication_log.date AS communication_date,
-    commun ication_log.message_details,
+    communication_log.communication_log_code,
+    communication_log.date_of_communication AS communication_date,
+    commun ication_log.details,
     supplier.supplier_name
 FROM
     employee  
@@ -18,4 +18,16 @@ ORDER BY
     employee.employee_id, communication logs.date;
 
 -- View for supplier details
-CREATE VIEW Supplier_View AS      
+CREATE VIEW Supplier_View AS
+SELECT 
+    supplier.supplierID, 
+    supplier.supplier_name, 
+    supplier.supplier_number, 
+    product.productID, 
+    product.product_name, 
+    product.product_group, 
+    product.product_available, 
+    communication_log.date_of_communication AS communication_date, 
+    communication_log.details, 
+    employee.employee_name, 
+    employee.employee_phone_number      
